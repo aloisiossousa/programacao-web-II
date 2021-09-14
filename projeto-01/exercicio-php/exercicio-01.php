002c65cb46bd55463f4714abe8c313d8d38b055b
Aloisio S Sousa <aloisio.ssousa@gmail.com>
2021-09-14 15:40:47 -0300

    Exercício 01 finalizado

---
+++ projeto-01/exercicio-php/exercicio-01.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 01 - Lista PHP 2021</title>
+</head>
+<body>
+    
+    <h2>Lista de Prática PHP - Exercício 01</h2>
+
+    <p>1 – Escreva um algoritmo para somar dois números e multiplicar o resultado pelo primeiro número.</p>
+    
+    <form name="form-01" method="post" action="exercicio-exec-01.php" >
+        <label for="num1">Digite o primeiro número.</label>
+        <input type="text" size="5" name="num1">
+        <label for="num2">Digite o segundo número.</label>
+        <input type="text" size="5" name="num2">
+       <input type="submit" name="" id="" value="Calcular">
+    </form>
+</body>
+</html>
